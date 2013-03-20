3e158917f7cea9efc0ee7333f362fe542f121da6
Diego Pino <dpino@igalia.com>
2013-03-20 12:56:37 +0100

    Add entity 'WordIndex'

---
+++ app/models/word_index.rb
@@ -0,0 +1,3 @@
+class WordIndex < ActiveRecord::Base
+  attr_accessible :indice, :word
+end
---
+++ test/unit/word_index_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class WordIndexTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
