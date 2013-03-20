6cf098356807fbff67ffa64d6d01fd16ee28791f
Diego Pino <dpino@igalia.com>
2013-03-20 12:54:34 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Galparl::Application.config.secret_token = 'bbb982456df5364f5a2f858acddf1e0de1055d4ef2e0ed8ef8271ccb21de15e01e9f004187f5a2fc5af1eb554fa6fca5a99102e21e09058973008dda5a26c5ea'
