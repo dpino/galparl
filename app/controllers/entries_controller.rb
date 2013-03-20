8d2ef007a211b3148032f2296ce64ace0891f220
Diego Pino <dpino@igalia.com>
2013-03-20 17:07:19 +0100

    Get entries by 'numid'

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -13,7 +13,7 @@ class EntriesController < ApplicationController
   # GET /entries/1
   # GET /entries/1.json
   def show
-    @entry = Entry.find(params[:id])
+    @entry = Entry.where(:numid => params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
