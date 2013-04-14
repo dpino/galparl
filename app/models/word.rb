be4ed2c68890ccdb343b987010bb74b657b36d8d
Diego Pino <dpino@igalia.com>
2013-04-14 20:17:59 +0200

    Group by speakers

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,11 +1,11 @@
 class Word 
-  # attr_accessible :count, :word, :indices, :entries, :error
+  # attr_accessible :count, :word, :indices, :speakers, :error
    
   def initialize(word)
     word_index = WordIndex.where(:word => word).first
 
     if (word_index != nil) 
-        @entries = getEntries(word_index.indice)
+        @speakers = groupBySpeakers(getEntries(word_index.indice))
         @count = WordCount.where(:word => word).first.count
         @word = word
     else 
@@ -16,6 +16,30 @@ class Word
   def getEntries(indice)
     indice = indice.split(",")
     return Entry.find(:all, :conditions => ['numid in (?)', indice])
+  end
+
+  def groupBySpeakers(entries)
+    speakers = Hash.new()
+    entries.each { |entry|
+        speaker = entry.person
+        if (speakers.has_key?(speaker))
+            bodies = speakers[speaker]
+        else
+            bodies = Array.new()     
+        end
+        bodies.push(encapsulateEntry(entry))
+        speakers[speaker] = bodies
+    }
+    return speakers
+  end
+
+  def encapsulateEntry(entry)
+    return {
+        :body => entry.body,
+        :date => entry.date,
+        :season => entry.season,
+        :numid => entry.numid
+    };
   end
 
 end
